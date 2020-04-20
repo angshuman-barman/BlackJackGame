9cfc7d73b1823fe7c4acedb046fa92ecd8151aa9
Angshuman Barman <48986353+angshuman-barman@users.noreply.github.com>
2020-04-20 22:19:29 +0530

    Add files via upload

---
+++ self.ace explanation.py
@@ -0,0 +1,12 @@
+#we are using self.aces integer value as a BOOLEAN in line 66
+
+zero=0 #Zero is treated as False
+one=1  #Other numbers are treadted as false
+two=2
+
+
+    
+if zero:
+    print("TRUE")
+else:
+    print("False")
